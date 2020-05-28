b52b23833e1649c3b8cf172b983ad0da985c38f5
Preuß <joshua.preuss2000@gmx.de>
2020-05-28 10:33:28 +0200

    first task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import Todos from "./data/todos.js";
 
 const App = () => {
 
 	return (
 		<div>
+
 			<h1>Todoooooooo app</h1>
 
-			<h3>Was ich machen werde wenn die kontaktsperre aufgehoben wird:</h3>
+			<h3>Was ich machen werde wenn die Kontaktsperre aufgehoben wird:</h3>
 			<ul>
 
-				<li>
-					Item
-				</li>
+				{Todos.map(function(item, itemID){
+					return (
+					<li key={itemID}>
+						{item.title}
+					</li>
+					)
+				})}
 
 			</ul>
+			
 		</div>
 	);
 };
